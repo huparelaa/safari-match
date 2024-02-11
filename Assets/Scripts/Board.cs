fda2d643e1f4a4c21af3ea8847d44c979d704cf3
Hobar <huparelaa@eafit.edu.co>
2024-02-11 11:06:50 -0500

    Camera position set

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,28 +8,46 @@ public class Board : MonoBehaviour
     public int height;
 
     public GameObject tileObject;
+
+    public float CameraSizeOffset;
+    public float CameraVerticalOffset;
+
     // Start is called before the first frame update
     void Start()
     {
         SetupBoard();
+        PositionCamera();
     }
 
     private void SetupBoard()
     {
-        for(int x = 0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
-                var o = Instantiate(tileObject, new Vector3(x,y,-5), Quaternion.identity);
+                var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
             }
         }
 
     }
 
+    private void PositionCamera()
+    {
+        float newPosx = (float)width / 2f;
+        float newPosy = (float)height / 2f;
+
+        Camera.main.transform.position = new Vector3(newPosx - 0.5f, newPosy - 0.5f + CameraVerticalOffset, -10f);
+
+        float horizontal = (float)width + 1f;
+        float vertical = (float)height / 2f + 1f;
+
+        Camera.main.orthographicSize = (horizontal > vertical) ? horizontal + CameraSizeOffset : vertical + CameraSizeOffset;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
