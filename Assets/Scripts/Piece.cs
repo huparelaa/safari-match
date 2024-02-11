da3c8562ecb7b722a3c7e71caadc1109dba6d3fd
Hobar <huparelaa@eafit.edu.co>
2024-02-11 12:24:13 -0500

    Add piece movement

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Piece : MonoBehaviour
 {
     public int x;
@@ -22,12 +22,27 @@ public class Piece : MonoBehaviour
         rabbit,
         snake
     }
-    public type pieceType; 
-    
+    public type pieceType;
+
     public void Setup(int x_, int y_, Board board_)
     {
         x = x_;
         y = y_;
         board = board_;
+    }
+
+    public void Move(int destinationX, int destinationY)
+    {
+        transform.DOMove(new Vector3(destinationX, destinationY, -5f), 0.25f).SetEase(Ease.InOutCubic).OnComplete(() =>
+        {
+            x = destinationX;
+            y = destinationY;
+        });
+    }
+
+    [ContextMenu("Move testing")]
+    public void MoveTest()
+    {
+        Move(0, 0);
     }
 }
